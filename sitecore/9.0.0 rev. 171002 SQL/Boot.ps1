8a85be0c03f650d93f5d77c292a628e3e4f099f6
Per Manniche Bering <pmbering@gmail.com>
2018-02-10 18:31:35 +0100

    fix sql reattach when container restarts, add sitecore scripts to PATH

--- sitecore/9.0.0 rev. 171002 SQL/Boot.ps1
+++ sitecore/9.0.0 rev. 171002 SQL/Boot.ps1
@@ -27,7 +27,7 @@ Get-ChildItem -Path $DataPath -Filter "*.mdf" | ForEach-Object {
     $databaseName = $_.BaseName.Replace("_Primary", "")
     $mdfPath = $_.FullName
     $ldfPath = $mdfPath.Replace(".mdf", ".ldf")
-    $sqlcmd = "CREATE DATABASE [$databaseName] ON (FILENAME = N'$mdfPath'), (FILENAME = N'$ldfPath') FOR ATTACH;"
+    $sqlcmd = "IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE NAME = '$databaseName') BEGIN EXEC sp_detach_db [$databaseName] END;CREATE DATABASE [$databaseName] ON (FILENAME = N'$mdfPath'), (FILENAME = N'$ldfPath') FOR ATTACH;"
 
     Write-Host "### Attaching '$databaseName'..."
 
