01e396d8a8459e11edf466a0006923a554d49d40
Serhii Shushliapin <sshushlyapin@gmail.com>
2019-02-04 09:38:27 +0100

    Add 9.0.2 based on Windows Server Core 1709 (#14)

---
+++ images/9.0.2 rev. 180604/windowsservercore-1709/sitecore-sqldev/Install-Databases.ps1
@@ -0,0 +1,43 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( {Test-Path $_ -PathType 'Container'})] 
+    [string]$InstallPath,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( {Test-Path $_ -PathType 'Container'})] 
+    [string]$DataPath,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()] 
+    [string]$DatabasePrefix
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
+$server.Properties["DefaultFile"].Value = $InstallPath
+$server.Properties["DefaultLog"].Value = $InstallPath
+$server.Alter()
+
+$sqlPackageExePath = Get-Item "C:\tools\*\lib\net46\SqlPackage.exe" | Select-Object -Last 1 -Property FullName -ExpandProperty FullName
+
+Write-Host "Using: $sqlPackageExePath"
+
+Push-Location -Path $InstallPath
+
+Get-ChildItem -Path $InstallPath -Filter "*.dacpac" | ForEach-Object {
+    $databaseName = $_.BaseName.Replace("Sitecore.", "$DatabasePrefix`_").Replace(".Database.Sql", ".ShardMapManager")
+    $dacpacPath = ".\{0}" -f $_.Name
+
+    # Install
+    & $sqlPackageExePath /a:Publish /sf:$dacpacPath /tdn:$databaseName /tsn:$env:COMPUTERNAME /q
+
+    # Detach
+    Invoke-Sqlcmd -Query "EXEC MASTER.dbo.sp_detach_db @dbname = N'$databaseName', @keepfulltextindexfile = N'false'"
+}
+
+Pop-Location
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
+$server.Properties["DefaultFile"].Value = $DataPath
+$server.Properties["DefaultLog"].Value = $DataPath
+$server.Alter()
