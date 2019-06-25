09586394f8538e65d877a832589eba157331ace1
Per Manniche Bering <per.bering@valtech.com>
2019-06-25 15:45:29 +0200

    added PSE on 9.2.0

---
+++ variants/9.2.0/windowsservercore/pse/sitecore-xp-pse-5.0-sqldev/Extract-Databases.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Test-Path $_ -PathType 'Container' })] 
+    [string]$Path
+)
+
+Add-Type -Assembly "System.IO.Compression.FileSystem";
+
+# extract module
+Get-ChildItem -Path $Path -Filter "*.zip" | ForEach-Object { 
+    Expand-Archive  $_.FullName -DestinationPath (Join-Path $Path $_.GetHashCode())
+}
