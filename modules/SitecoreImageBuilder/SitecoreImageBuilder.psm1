563dd0d634aa85edf59e309024b88e7ef2ce764e
Per Manniche Bering <per.bering@valtech.com>
2019-09-24 10:58:51 +0200

    fixed more AutoGenerateWindowsVersionTags paramters

--- modules/SitecoreImageBuilder/SitecoreImageBuilder.psm1
+++ modules/SitecoreImageBuilder/SitecoreImageBuilder.psm1
@@ -426,8 +426,8 @@ function Get-BuildSpecifications
         [ValidateScript( { Test-Path $_ -PathType "Container" })] 
         [string]$Path
         ,
-        [Parameter(Mandatory = $true)]
-        [array]$AutoGenerateWindowsVersionTags
+        [Parameter(Mandatory = $false)]
+        [array]$AutoGenerateWindowsVersionTags = (Get-SupportedWindowsVersions)
     )
 
     Get-ChildItem -Path $Path -Filter "build.json" -Recurse | ForEach-Object {
@@ -580,11 +580,14 @@ function Get-CurrentImages
         [Parameter(Mandatory = $true)]
         [ValidateScript( { Test-Path $_ -PathType "Container" })] 
         [string]$Path
+        ,
+        [Parameter(Mandatory = $false)]
+        [array]$AutoGenerateWindowsVersionTags = (Get-SupportedWindowsVersions)
     )
     
     $tagParser = [regex]"^(?<repository>[^:]*):(?<version>[^-]*)-(?<os>[^-]+)(?:-(?<build>.*))?$"
 
-    Get-BuildSpecifications -Path $Path | ForEach-Object {
+    Get-BuildSpecifications -Path $Path -AutoGenerateWindowsVersionTags $AutoGenerateWindowsVersionTags | ForEach-Object {
         $spec = $_
         $match = $tagParser.Match($spec.Tag)
 
@@ -615,12 +618,15 @@ function Get-CurrentImagesMarkdown
         [Parameter(Mandatory = $true)]
         [ValidateScript( { Test-Path $_ -PathType "Container" })] 
         [string]$Path
+        ,
+        [Parameter(Mandatory = $false)]
+        [array]$AutoGenerateWindowsVersionTags = (Get-SupportedWindowsVersions)
     )
     
     Write-Output "| Version | Repository | OS  | Build      | Tag |"
     Write-Output "| ------- | ---------- | --- | -----------| --- |"
 
-    Get-CurrentImages -Path $Path | Sort-Object -Property Version, Build, Repository -Descending | ForEach-Object {
+    Get-CurrentImages -Path $Path -AutoGenerateWindowsVersionTags $AutoGenerateWindowsVersionTags | Sort-Object -Property Version, Build, Repository -Descending | ForEach-Object {
         $dockerFileUrl = (Resolve-Path $_.DockerFilePath -Relative).Replace(".\", "").Replace("\", "/").Replace(" ", "%20")
 
         if ($_.Deprecated)
