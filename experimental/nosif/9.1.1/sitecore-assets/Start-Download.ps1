c4fdac9bf28b47b0063811bffaff0d4f5974011a
Per Manniche Bering <per.bering@valtech.com>
2019-09-25 11:34:36 +0200

    asset downloads will now retry each download up 5 times

--- experimental/nosif/9.1.1/sitecore-assets/Start-Download.ps1
+++ experimental/nosif/9.1.1/sitecore-assets/Start-Download.ps1
@@ -18,6 +18,71 @@ param(
     [string]$SitecorePassword
 )
 
+function Invoke-Download
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()] 
+        [string]$Url
+        ,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()] 
+        [string]$Destination
+        ,
+        [Parameter(Mandatory = $false)]
+        [int]$RetryCount = 0
+        ,
+        [Parameter(Mandatory = $false)]
+        [int]$MaxRetryCount = 5
+        ,
+        [Parameter(Mandatory = $false)]
+        [object]$Session
+    )
+
+    if (Test-Path $Destination) 
+    {
+        Remove-Item $Destination -Force
+    }
+    
+    try
+    {
+        Invoke-WebRequest -Uri $Url -OutFile $Destination -WebSession $Session -UseBasicParsing -TimeoutSec 120
+    }
+    catch
+    {
+        $exception = $_.Exception
+
+        if ($null -ne $exception)
+        {
+            $response = $exception.Response
+
+            if ($null -ne $response -and [int]$response.StatusCode -gt 400)
+            {
+                throw [Exception] "Download failed.", $exception
+            }
+            
+            if ($exception.Message -like "*timed out*" -or $exception.GetType() -eq [System.Threading.Tasks.TaskCanceledException])
+            {
+                if ($RetryCount -ge $MaxRetryCount)
+                {
+                    throw [Exception] "Download timed out, will not retry.", $exception
+                }
+                
+                Write-Warning "Retrying ($RetryCount/$MaxRetryCount)..."
+
+                Invoke-Download -Url $Url -Destination $Destination -Session $Session -RetryCount ($RetryCount + 1) -MaxRetryCount $MaxRetryCount
+            }
+
+            throw [Exception] "Unknown exception.", $exception
+        }
+        else
+        {
+            throw "Unknown error."
+        }
+    }
+}
+
 # Setup
 $sitecoreDownloadUrl = "https://dev.sitecore.net"
 $sitecoreDownloadSession = $null
@@ -38,13 +103,13 @@ Get-Content -Raw $Path | ConvertFrom-Json -PipelineVariable jo | Get-Member -Typ
     # if needed, login to dev.sitecore.net and save session for re-use
     if ($null -eq $sitecoreDownloadSession)
     {
-        Write-Host ("Logging in to '{0}' using username '{1}'..." -f $sitecoreDownloadUrl, $SitecoreUsername)
+        Write-Host ("Logging in to '{0}' using '{1}'..." -f $sitecoreDownloadUrl, $SitecoreUsername)
 
         $loginResponse = Invoke-WebRequest ("{0}/api/authorization" -f $sitecoreDownloadUrl) -Method Post -Body @{
             username   = $SitecoreUsername
             password   = $SitecorePassword
             rememberMe = $true
-        } -SessionVariable "sitecoreDownloadSession" -UseBasicParsing
+        } -SessionVariable "sitecoreDownloadSession" -Timeout 120 -UseBasicParsing
 
         if ($null -eq $loginResponse -or $loginResponse.StatusCode -ne 200 -or $loginResponse.Content -eq "false")
         {
@@ -54,7 +119,7 @@ Get-Content -Raw $Path | ConvertFrom-Json -PipelineVariable jo | Get-Member -Typ
     
     Write-Host ("Downloading '{0}' from '{1}'..." -f $fileName, $fileUrl)
 
-    Invoke-WebRequest -Uri $fileUrl -OutFile $filePath -WebSession $sitecoreDownloadSession -UseBasicParsing
+    Invoke-Download -Url $fileUrl -Destination $filePath -Session $sitecoreDownloadSession
 }
 
 Write-Host "Downloads completed."
