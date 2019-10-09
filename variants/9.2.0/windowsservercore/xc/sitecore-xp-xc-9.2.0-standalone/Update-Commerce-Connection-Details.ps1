39e18b82548c6a17f66e0e6ad25f3ce533d6061d
Arjen van Veen <arjen.van.veen@efocus.nl>
2019-10-09 23:08:39 +0200

    Updates for sitecore-xp-xc-9.2.0-standalone dockerfile

--- variants/9.2.0/windowsservercore/xc/sitecore-xp-xc-9.2.0-standalone/Update-Commerce-Connection-Details.ps1
+++ variants/9.2.0/windowsservercore/xc/sitecore-xp-xc-9.2.0-standalone/Update-Commerce-Connection-Details.ps1
@@ -12,8 +12,63 @@ param(
 	[Parameter(Mandatory = $true)]
 	[string]$CommerceServicesShopCurrency,
 	[Parameter(Mandatory = $true)]
-	[string]$CommerceServicesCertificateThumbprint
-)      
+	[string]$CommerceServicesCertificateId,
+	[Parameter(Mandatory = $true)]
+	[string]$CommerceServicesCertificateStorePath
+)   
+
+Function GetCertificate {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Id,
+        [ValidateScript( { $_ -like 'cert:\*' })]
+        [ValidateScript( { Test-Path $_ })]
+        [string]$CertStorePath = 'Cert:\LocalMachine\My'
+    )
+
+    Write-Verbose -Message $PSCmdlet.MyInvocation.MyCommand
+    Write-Verbose -Message "Id: $Id"
+    Write-Verbose -Message "CertStorePath: $CertStorePath"
+
+    $date = Get-Date
+
+    # Find all children under the path
+    $found = @(Get-ChildItem -Path $CertStorePath -Recurse |
+        # That are certificates
+        Where-Object { $_.GetType() -eq [System.Security.Cryptography.X509Certificates.X509Certificate2] } |
+        # That match thumbprint or dns name
+        Where-Object {
+            # Test as thumbprint
+            $match = $_.Thumbprint -eq $Id
+            if (!$match) {
+                # Test as name
+                $match = $Id -eq $_.GetNameInfo([System.Security.Cryptography.X509Certificates.X509NameType]::SimpleName, $false)
+            }
+
+            return $match
+        } |
+        # That are in date
+        Where-Object {
+            $date -ge $_.NotBefore -and $date -le $_.NotAfter
+        } |
+        # Get Latest
+        Sort-Object -Property NotAfter -Descending)    
+
+    if($found.Count -gt 0) {
+        Write-Verbose -Message "Found Cert(s) with thumbprint: $($found.Thumbprint)"
+    } else {
+        Write-Warning "Could not find Cert: $Id in $CertStorePath"
+        return $null
+    }
+
+    if($found.Count -gt 1) {
+        Write-Warning "Found multiple certificates, selecting certificate with longest expiry."
+    }
+
+    return $found | Select-Object -First 1 
+}
+
+$cert = GetCertificate -Id $CommerceServicesCertificateId -CertStorePath $CommerceServicesCertificateStorePath
 
 $pathToConfig = $(Join-Path -Path $EngineConnectIncludeDir -ChildPath "\Sitecore.Commerce.Engine.Connect.config") 
 $xml = [xml](Get-Content $pathToConfig)
@@ -50,5 +105,5 @@ $xml.Save($pathToConfig)
 
 $xml = [xml](Get-Content $pathToConfig)
 $node = $xml.configuration.sitecore.commerceEngineConfiguration
-$node.certificateThumbprint = $CommerceServicesCertificateThumbprint
+$node.certificateThumbprint = $($cert.Thumbprint)
 $xml.Save($pathToConfig) 
