08e8902bfcef1693f36375aa923aaa8fb4125648
Jean-François Larente <jfl@sitecore.net>
2019-10-17 10:07:36 -0400

    Initial add of 9.3

---
+++ windows/9.3.x/sitecore-xm-sqldev/Install-Databases.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Test-Path $_ -PathType 'Container' })]
+    [string]$InstallPath,
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( { Test-Path $_ -PathType 'Container' })]
+    [string]$DataPath
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
+$server.Properties["DefaultFile"].Value = $InstallPath
+$server.Properties["DefaultLog"].Value = $InstallPath
+$server.Alter()
+
+$sqlPackageExePath = Get-Item "C:\Program Files\Microsoft SQL Server\*\DAC\bin\SqlPackage.exe" | Select-Object -Last 1 -Property FullName -ExpandProperty FullName
+
+Write-Host "Using: $sqlPackageExePath"
+
+Push-Location -Path $InstallPath
+
+Get-ChildItem -Path $InstallPath -Filter "*.dacpac" | ForEach-Object {
+    $databaseName = $_.BaseName
+    $dacpacPath = ".\{0}" -f $_.Name
+
+    # Install
+    & $sqlPackageExePath /a:Publish /sf:$dacpacPath /tdn:$databaseName /tsn:$env:COMPUTERNAME /q
+
+    # Detach
+    Invoke-Sqlcmd -Query "EXEC MASTER.dbo.sp_detach_db @dbname = N'$databaseName', @keepfulltextindexfile = N'false'"
+}
+
+Pop-Location
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
+$server.Properties["DefaultFile"].Value = $DataPath
+$server.Properties["DefaultLog"].Value = $DataPath
+$server.Alter()
