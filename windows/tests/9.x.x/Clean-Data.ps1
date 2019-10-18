3734aa53adc4986b3d2e74056f2d58d0bde50c75
Per Manniche Bering <per.bering@valtech.com>
2019-10-18 15:42:06 +0200

    better clean-data

--- windows/tests/9.x.x/Clean-Data.ps1
+++ windows/tests/9.x.x/Clean-Data.ps1
@@ -1,7 +1,5 @@
 Get-ChildItem -Path (Join-Path $PSScriptRoot "\data") -Directory | ForEach-Object {
     $dataPath = $_.FullName
 
-    Get-ChildItem -Path $dataPath | ForEach-Object {
-        Get-ChildItem -Path $_.FullName -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
-    }
+    Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
 }
