91ddda31cf23d9aa63554cb4f00c73b51f8b5efa
Bart Plasmeijer <bart.plasmeijer@gmail.com>
2019-12-09 11:52:33 +0100

    add ExperimentalTagBehavior for PS (#163)
    * add ExperimentalTagBehavior for PS
    
    update readme with ExperimentalTagBehavior for PS
    
    * remove --no-cache

--- Download-PS-Prerequisites .ps1
+++ Download-PS-Prerequisites .ps1
@@ -11,24 +11,24 @@ param(
     ,
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    [string]$SitecoreUsername 
+    [string]$SitecoreUsername
     ,
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    [string]$SitecorePassword 
-   
+    [string]$SitecorePassword
+
 
 )
 
 $sitecoreDownloadUrl = "https://dev.sitecore.net"
 
-$packages = @{"Sitecore Azure Toolkit 2.4.0-r02514.1001.zip"  = "https://dev.sitecore.net/~/media/B627BFA7070B40AD854A0FF08719381D.ashx" 
+$packages = @{"Sitecore Azure Toolkit 2.4.0-r02514.1001.zip"  = "https://dev.sitecore.net/~/media/B627BFA7070B40AD854A0FF08719381D.ashx"
     "Sitecore Publishing Module 9.3.0.0 rev. r00546.2197.zip" = "https://dev.sitecore.net/~/media/417C43AC4F334C4085D18925367FE79A.ashx"
-} 
+}
 
 # download packages from Sitecore download
-$packages.GetEnumerator() | ForEach-Object { 
-    
+$packages.GetEnumerator() | ForEach-Object {
+
     $filePath = Join-Path $InstallSourcePath $_.Key
     $fileUrl = $_.Value
 
@@ -69,31 +69,33 @@ $packages.GetEnumerator() | ForEach-Object {
 
 }
 
+$Destination = Join-Path $PSScriptRoot "packages"
+
 # Install Azure toolkit
 Write-Host "Prepare Azure toolkit"
 
 $sat = (Join-Path (Get-Item $PSScriptRoot).FullName "tools\sat")
 
-Write-Host "Sitecore Azure Toolkit directory $sat"  
+Write-Host "Sitecore Azure Toolkit directory $sat"
 
 # Ensure Azure SAT destination exists
 if (!(Test-Path $sat -PathType "Container")) {
-    Write-Host "Create SAT directory $sat"  
+    Write-Host "Create SAT directory $sat"
     New-Item $sat -ItemType Directory -WhatIf:$false | Out-Null
 }
-    
+
 # extract Sitecore Azure Toolkit
 Get-ChildItem -Path $Destination -Filter "*Azure Toolkit*" -Recurse | Select-Object -First 1 | Expand-Archive -DestinationPath $sat -Force
 
 # import Azure toolkit
-Write-Host "Import Sitecore Azure Toolkit"  
+Write-Host "Import Sitecore Azure Toolkit"
 Import-Module (Join-Path $sat "tools\Sitecore.Cloud.Cmdlets.dll")  -Force
 
-# Find publishing ,odule 
-$zips = Get-ChildItem -Recurse -Path $Destination -Include "Sitecore Publishing Module*" -Exclude "*scwdp*" 
+# Find publishing ,odule
+$zips = Get-ChildItem -Recurse -Path $Destination -Include "Sitecore Publishing Module*" -Exclude "*scwdp*"
 
-$zips | ForEach-Object { 
-    Write-Host "Convert to WDP $_.FullName"
+$zips | ForEach-Object {
+    Write-Host "Convert to WDP $_"
     ConvertTo-SCModuleWebDeployPackage -Path $_ -Destination $Destination -Force
 }
 
