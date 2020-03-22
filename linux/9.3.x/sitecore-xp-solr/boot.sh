47014022e6ea274b9063cbfe9fe697f368f6311c
Bart Plasmeijer <bap@sitecore.net>
2020-03-22 16:36:30 +0100

    remove asyncRun from boot.sh

--- linux/9.3.x/sitecore-xp-solr/boot.sh
+++ linux/9.3.x/sitecore-xp-solr/boot.sh
@@ -19,18 +19,6 @@ else
     find $dataDir/. -name write.lock -print0 | xargs -0 rm -rf
 fi
 
-asyncRun() {
-    "$@" &
-    pid="$!"
-    trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
-
-    # A signal emitted while waiting will make the wait command return code > 128
-    # Let's wrap it in a loop that doesn't end before the process is indeed stopped
-    while kill -0 $pid > /dev/null 2>&1; do
-        wait
-    done
-}
-
 echo "### Starting Solr in background..."
 
 /opt/docker-solr/scripts/docker-entrypoint.sh solr-foreground &
