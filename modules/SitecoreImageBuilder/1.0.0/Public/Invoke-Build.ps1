33fb781404d3d568836ccbb7826c8d15758d93b5
Ramon Brülisauer <ramon.bruelisauer@namics.com>
2020-04-01 10:52:05 +0200

    #256: Add IsolationMode parameter to build.ps1 (#261)
    * #256: Add IsolationMode parameter to build.ps1
    
    * #256: handle IsolationMode = unspecified
    
    * #256: Change IsolationMode parameter into IsolationModeBehaviour
    
    * Sitecore#256: Change IsolationMode parameter into IsolationModeBehaviour
    
    * #256: use --isolation 'default' on non-windows OS, instead of --isolation 'process'.

--- modules/SitecoreImageBuilder/1.0.0/Public/Invoke-Build.ps1
+++ modules/SitecoreImageBuilder/1.0.0/Public/Invoke-Build.ps1
@@ -56,6 +56,10 @@ function Invoke-Build
         ,
         [Parameter(Mandatory = $false)]
         [switch]$SkipHashValidation
+        ,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("ForceHyperV", "EngineDefault", "ForceProcess", "ForceDefault")]
+        [string]$IsolationModeBehaviour = "ForceHyperV"
     )
 
     # Setup
@@ -81,7 +85,7 @@ function Invoke-Build
     # Print results
     $specs | Select-Object -Property Tag, Include, Deprecated, Priority, Base | Format-Table
 
-    # Determine OS
+    # Determine OS (windows or linux)
     $osType = (docker system info --format '{{json .}}' | ConvertFrom-Json | ForEach-Object { $_.OSType })
 
     Write-Message "Build specifications loaded..." -Level Info
@@ -197,9 +201,20 @@ function Invoke-Build
             # Build image
             $buildOptions = New-Object System.Collections.Generic.List[System.Object]
 
-            if ($osType -eq "windows")
-            {
+            if ($osType -ieq "windows" -and $IsolationModeBehaviour -ieq "ForceHyperV") {
+                # --isolation 'hyperv' | makes sense on windows host only?
                 $buildOptions.Add("--isolation 'hyperv'")
+            }
+            elseif ($osType -ieq "windows" -and $IsolationModeBehaviour -ieq "ForceProcess") {
+                # --isolation 'process' | works only on windows
+                $buildOptions.Add("--isolation 'process'")
+            }
+			elseif ($osType -ne "windows" -and $IsolationModeBehaviour -ieq "ForceDefault") {
+				# --isolation 'default' | works on non-windows
+				$buildOptions.Add("--isolation 'default'")
+			}
+            else {
+                # no --isolation option | also use engine default if none of the above has been selected
             }
 
             $spec.BuildOptions | ForEach-Object {
