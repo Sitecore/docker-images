f233390a8ae15f11543a316624113a12a5dc695a
Vincent Lui <vincent.tat.man.lui@gmail.com>
2020-12-04 21:36:57 +1100

    Removing XM1 compose files

--- build/windows/tests/10.0.x/sitecore-xm1/clean.ps1
+++
@@ -1,3 +0,0 @@
-# Clean data folders
-Get-ChildItem -Path (Join-Path $PSScriptRoot "\mssql-data") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
-Get-ChildItem -Path (Join-Path $PSScriptRoot "\solr-data") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
